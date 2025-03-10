5aea77e73320bb75873f759f6a022cf678fc842c
shivam_chaudhari19 <shivamsc7289@gmail.com>
2025-03-10 22:08:21 +0530

    fetc data 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,49 +15,49 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args)  throws Exception {
 
-//        save Image
-        String city="Chhatrapati Sambhajinagar";
-        String street="Railway station road Chh. Sambhajinagar";
-        boolean isOpen=true;
-        Date date=new Date();
-
-        FileInputStream fis= new FileInputStream("src/main/resources/prof.jpg");
-        byte[] image=new byte[fis.available()];
-        fis.read(image);
-        Address address= new Address();
-        address.setStreet(street);
-        address.setCity(city);
-        address.setOpen(isOpen);
-        address.setAddedDate(date);
-        address.setImage(image);
-        Session session=new Configuration()
-                .configure()
-                .addAnnotatedClass(model.Address.class)
-                .buildSessionFactory()
-                .openSession();
-        Transaction transaction=null;
-        try {
-            transaction=session.beginTransaction();
-            session.persist(address);
-            transaction.commit();
-            session.close();
-        }catch (Exception e){
-            if (transaction!=null)
-            {
-                transaction.rollback();
-            }
-            e.printStackTrace();
-        }
-        session.close();
+////        save Image
+//        String city="Chhatrapati Sambhajinagar";
+//        String street="Railway station road Chh. Sambhajinagar";
+//        boolean isOpen=true;
+//        Date date=new Date();
+//
+//        FileInputStream fis= new FileInputStream("src/main/resources/prof.jpg");
+//        byte[] image=new byte[fis.available()];
+//        fis.read(image);
+//        Address address= new Address();
+//        address.setStreet(street);
+//        address.setCity(city);
+//        address.setOpen(isOpen);
+//        address.setAddedDate(date);
+//        address.setImage(image);
+//        Session session=new Configuration()
+//                .configure()
+//                .addAnnotatedClass(model.Address.class)
+//                .buildSessionFactory()
+//                .openSession();
+//        Transaction transaction=null;
+//        try {
+//            transaction=session.beginTransaction();
+//            session.persist(address);
+//            transaction.commit();
+//            session.close();
+//        }catch (Exception e){
+//            if (transaction!=null)
+//            {
+//                transaction.rollback();
+//            }
+//            e.printStackTrace();
+//        }
+//        session.close();
 
 
 
 //        Fetch data
-//        Students students;
-//        SessionFactory factory= HibernateUtils.getSessionFactory();
-//        Session session=factory.openSession();
-//        students=session.find(Students.class,"1");
-//        System.out.println(students);
+        Students students;
+        SessionFactory factory= HibernateUtils.getSessionFactory();
+        Session session=factory.openSession();
+        students=session.byId(Students.class).load();
+        System.out.println(students);
 
     }
 }
